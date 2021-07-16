d77a34174204dc3844d92660c9a56a1b6fd318cd
Bumble <nick1092387456@gmail.com>
2021-07-16 23:43:38 +0800

    add search, each indexe.hbs ,show.hbs. Finish

---
+++ app.js
@@ -0,0 +1,47 @@
+//Include express from node_modules
+const express = require("express");
+const app = express();
+
+//載入餐廳Json檔
+const restaurantList = require("./restaurant.json");
+
+//Include handlebars
+const exphbs = require("express-handlebars");
+
+//Define server related variables
+const port = 3000;
+
+//setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//setting static files
+app.use(express.static("public"));
+
+//setting route and response
+//index
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+//show
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+//search
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLocaleLowerCase());
+  });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
+});
+
+//Start and listen the server
+app.listen(port, () => {
+  console.log(`Server is started`);
+});
