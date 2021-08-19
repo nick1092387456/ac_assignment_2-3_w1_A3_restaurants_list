8fbaea85ab23b02f75f5809c1c274d6e02ef1764
Bumble <nick1092387456@gmail.com>
2021-08-19 17:58:20 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const lists = require('./modules/lists')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/lists', lists)
+router.use('/users', users)
 
 module.exports = router
