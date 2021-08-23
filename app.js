b6dbc7bc759e90209ee86005ff746d3033776eb8
Bumble <nick1092387456@gmail.com>
2021-08-24 01:11:22 +0800

    feat: add mregister error message partails

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 require('./config/mongoose.js')
@@ -23,9 +24,12 @@ app.use(
   })
 )
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
