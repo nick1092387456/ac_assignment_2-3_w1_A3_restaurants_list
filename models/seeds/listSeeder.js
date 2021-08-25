893cdfb819365c85fa8d0ba40fb5076f2fcf127d
Bumble <nick1092387456@gmail.com>
2021-08-25 17:13:01 +0800

    modify listSeeder

--- models/seeds/listSeeder.js
+++ models/seeds/listSeeder.js
@@ -1,24 +1,55 @@
 //載入資料
-const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const List = require('../list')
+const User = require('../../models/user')
 const data = require('../../restaurant.json')
 const restaurantList = data.results
-const List = require('../list')
+const db = require('../../config/mongoose')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '123456',
+}
 
 //建立資料
 db.once('open', () => {
   console.log('mongodb connected!')
-  restaurantList.forEach((restaurant) => {
-    List.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description,
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id
+      console.log('generating list...')
+      return Promise.all(
+        Array.from(restaurantList, (restaurant) => {
+          return List.create({
+            userId,
+            name: restaurant.name,
+            name_en: restaurant.name_en,
+            category: restaurant.category,
+            image: restaurant.image,
+            location: restaurant.location,
+            phone: restaurant.phone,
+            google_map: restaurant.google_map,
+            rating: restaurant.rating,
+            description: restaurant.description,
+          })
+        })
+      )
     })
-  })
-  console.log('done')
+    .then(() => {
+      console.log('seeder done.')
+      process.exit()
+    })
 })
